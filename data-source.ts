581fbdeb0284409646ade8fd21c69e5d3dca5f82
Hamza Akhtar <hamzaakhtar008@gmail.com>
2025-10-03 15:14:58 +0500

    Changes

--- data-source.ts
+++ data-source.ts
@@ -1,13 +1,18 @@
 import { DataSource } from 'typeorm';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 export default new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'hamza',
-  password: '1234',
-  database: 'cafe_db',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: [__dirname + '/src/entities/*.entity.{ts,js}'],
   migrations: [__dirname + '/src/migrations/*.{ts,js}'],
   synchronize: false,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false, // Enabling SSL based on environment variable
 });
